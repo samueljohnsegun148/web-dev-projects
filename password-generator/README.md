fa737d415ec9e645e20acf3c61b9842e9fc57489
samueljohnsegun148 <samueljohnsegun148@gmail.com>
2024-02-13 17:38:40 +0100

    Add scripts for password generator to main

---
+++ password-generator/README.md
@@ -0,0 +1,34 @@
+# Project Tittle
+## Password Generator
+
+## LICENSE
+MIT License
+
+Copyright (c) [2024] [SJS]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Project Description
+This web app generates a password when the button is clicked. The user receives a series of prompts for password criteria such as the lenght of the password, which must satisfy the minimum 8 characters and maximum 128 characters criteria. The user should atleast select one of the four character options to be included. These options are lowercase, uppercase, numeric and special characters.
+
+Once the criteria are fulfilled, the password would be generated and displayed on the page for the user.
+
+## Links
+'This site is built using [GitHub Pages]().'
+
---
+++ password-generator/script.js
@@ -0,0 +1,165 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.'
+  ];
+  
+  // Array of numeric characters to be included in password
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  
+  // Array of lowercase characters to be included in password
+  var lowerCasedCharacters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  
+  // Array of uppercase characters to be included in password
+  var upperCasedCharacters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
+// declare variables for the script
+let promptPasswordLenght;
+let preferUpperCase
+let preferLowerCase;
+let preferNumericChar;
+let preferSpecialChar;
+let randomValue;
+
+// function to get password options
+function getPasswordOptions() {
+    promptPasswordLenght =  prompt("What is your preferred password lenght?");
+    if (promptPasswordLenght >= 8 && promptPasswordLenght <= 128){
+        preferUpperCase = confirm("Would you like your password to include uppercase characters?");
+        preferLowerCase = confirm("Would you like your password to include lowercase characters?");
+        preferNumericChar = confirm("Would you like your password to include numeric characters?");
+        preferSpecialChar = confirm("Would you like your password to include special characters?");
+    } 
+    else {
+        alert("Invalid password lenght! Value must be from 8 - 128 !");
+        getPasswordOptions();
+    }
+
+    // check if at least one character type is selected, else prompt the user to select at least a char type
+    if (!(preferUpperCase || preferLowerCase || preferNumericChar || preferSpecialChar)) {
+        alert("Your password must include at least one of the four character types!");
+        getPasswordOptions();
+    }
+}
+
+// function to get random number
+function getRandom(arr){
+    randomValue = Math.floor(Math.random() * (arr.length));
+    return randomValue;
+ }
+
+// function to generate password based on user criteria 
+function generatePassword () {
+    getPasswordOptions(); // get user's password preferences
+    let password = ""
+    for (let i = 0; i < promptPasswordLenght; i++) {
+        if (preferUpperCase && password.length < promptPasswordLenght) {
+            password += upperCasedCharacters[getRandom(upperCasedCharacters)];
+        }
+        if (preferLowerCase && password.length < promptPasswordLenght) {
+            password += lowerCasedCharacters[getRandom(lowerCasedCharacters)];
+        }
+        if (preferNumericChar && password.length < promptPasswordLenght) {
+            password += numericCharacters[getRandom(numericCharacters)];
+        }
+        if (preferSpecialChar && password.length < promptPasswordLenght) {
+            password += specialCharacters[getRandom(specialCharacters)];
+        }
+    } 
+    return password;  
+}
+
+// give reference to the #generate button element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+    var generatedPassword = generatePassword();
+    var passwordText = document.querySelector('#password');
+    passwordText.value = generatedPassword;
+}
+
+// add event listener to generate button
+generateBtn.addEventListener('click', writePassword)
+
+
+
+
+
+
+
+
